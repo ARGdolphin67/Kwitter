0a146309076ec5b0541c705567c2011355790de8
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2022-12-06 11:21:52 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -36,3 +36,9 @@ function rederectToRoomName(name){
       localStorage.setItem("room_name", name);
       window.location="kwitter_page.html";
 }
+
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
